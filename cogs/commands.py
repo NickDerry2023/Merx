8146aa0aeea23926c7b46bc5305d709af3a709d7
NickDerry2023 <nickd@caliwebdesignservices.com>
2024-09-16 08:43:48 +0000

    [BUGFIX] Removed comment for testing.

--- cogs/commands.py
+++ cogs/commands.py
@@ -7,8 +7,6 @@ from cogs.utils.constants import MerxConstants
 from cogs.utils.embeds import AboutEmbed, AboutWithButtons
 
 
-# The main commands Cog.
-
 class CommandsCog(commands.Cog):
     def __init__(self, merx):
         self.merx = merx
