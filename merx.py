f38ec9ea02cc4ff7b3ab81f41b8cea0111fcbea0
NickDerry2023 <nickd@caliwebdesignservices.com>
2024-09-28 14:19:35 +0000

    [UPDATE] Chnaged wording to be more proper. Used proper uppercase and lowercase.

--- merx.py
+++ merx.py
@@ -104,7 +104,7 @@ async def global_blacklist_check(ctx):
         
         em = discord.Embed(
             title="Blacklisted",
-            description="You are blacklisted from Merx - please appeal within our [support server](https://discord.gg/nAX4yhVEgy)!",
+            description="This server is blacklisted from Merx - Please appeal within our [Support Server](https://discord.gg/nAX4yhVEgy)!",
             color=constants.merx_embed_color_setup(),
         )
         
@@ -119,7 +119,7 @@ async def global_blacklist_check(ctx):
         
         em = discord.Embed(
             title="Blacklisted Guild",
-            description="This server is blacklisted from Merx - please appeal within our [support server](https://discord.gg/nAX4yhVEgy)!",
+            description="This server is blacklisted from Merx - Please appeal within our [Support Server](https://discord.gg/nAX4yhVEgy)!",
             color=constants.merx_embed_color_setup(),
         )
         
